f47d3a06189461d148e5bd998eb639314ca919ab
Nikhil Chaughule <nikhil.chaughule@inscripts.in>
2016-12-14 17:15:56 +0530

    Finalized Logic

--- assets/app.js
+++ assets/app.js
@@ -1,8 +1,21 @@
 var app = angular.module('kywd', []);
 
 app.controller('MainCtrl', function($scope,$timeout) {
-  $scope.name = '';
-  $scope.$watch('name', function() {
-        $scope.name = $scope.name.toLowerCase().replace(/[^a-zA-Z0-9_]/g,'_');
-  });
+	$scope.name = '';
+	$scope.prefix = '';
+	$scope.output=''
+	$scope.$watch('[name,prefix]', function() {
+        $scope.output = TextDefine($scope.prefix.toLowerCase().replace(/[^a-zA-Z0-9_]/g,'_'),$scope.name.toLowerCase().replace(/[^a-zA-Z0-9_\r\n]/g,'_'));
+	});
 });
+
+function TextDefine(prefix,val) {
+	if(!(prefix && val))
+		return "";
+	var i = 0;
+	var array1 = val.split("\n");
+	for (i = 0; i < array1.length; i++) {
+		array1[i] = (prefix +'_'+ array1[i]).trim();
+	}
+	return array1.join("\n");
+}
